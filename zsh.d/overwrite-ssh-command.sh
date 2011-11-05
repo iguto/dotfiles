cff2e64649dcc8b2bbe4fd762a22ec8b9a064c79
s09t214 OOKAWA Masahiro <ookawa@poulenc.eng.kagawa-u.ac.jp>
2011-11-06 03:51:57 +0900

    tmp

--- zsh.d/overwrite-ssh-command.sh
+++ zsh.d/overwrite-ssh-command.sh
@@ -1,38 +1,45 @@
 ############################################################
 #  ssh-agentをssh系コマンド実行前に実行
 ############################################################
+
+# 設定のロード
+echo $zsh_dir > /dev/null
+if [ $? -ne 0 ]; then
+	echo "can't load \$zsh_dir" > /dev/stderr
+fi
+
 ## ssh-agent をssh,scp,sftp,rsyncに関連付け
 # my-ssh
-if [ -e ~/.zshd/run-agent ]; then
+if [ -e $zsh_dir/run-agent ]; then
   function ssh {
-    source ~/.zshd/run-agent
+    source $zsh_dir/run-agent
     run-agent
     /usr/bin/ssh $*
   }
   #my-scp
 # function scp {
-#   source ~/.zshd/run-agent
+#   source $zsh_dir/run-agent
 #   run-agent
 #   /usr/local/bin/scp $*
 # }
 # # my-sftp
 # function sftp {
-#   source ~/.zshd/run-agent
+#   source $zsh_dir/run-agent
 #   run-agent
 #   /usr/local/bin/sftp $*
 # }
   # my-rsync
   function my-rsync {
-    source ~/.zshd/run-agent
+    source $zsh_dir/run-agent
     run-agent
     /usr/bin/rsync $*
   }
   function agent {
-    source ~/.zshd/run-agent
+    source $zsh_dir/run-agent
     run-agent
   }
   # function screen {
-  #   source ~/.zshd/run-agent
+  #   source $zsh_dir/run-agent
   #   run-agent
   #   /usr/bin/screen $*
   # }
