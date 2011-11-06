248a8db784dc25d07cea967199c6e7d745fae912
iguto <clc.iguto@gmail.com>
2011-11-06 13:49:25 +0900

    PROMPT second line change more shoter

--- zsh.d/ip_prompt.zsh
+++ zsh.d/ip_prompt.zsh
@@ -186,7 +186,7 @@ set_color () {
 	PROMPT=$'%{$GREEN%}[%{$fg[${U_CLR}]%}${USER}%{${GREEN}%}@%{$fg[${H_CLR}]%}${HOST}%{${GREEN}%}(%{$fg["yellow"]%}${inet_addr}%{$fg['green']%}):%{$fg['blue']%}%${user_host_decolation_size}<...<${cwd}%<<%{$fg['green']%}]%{$fg['cyan']%}'
 	fill_char
 
-	s_line_f="〓―(%#"
+	s_line_f="-(%#"
 	# $?
 	s_line_l=")->> "
 
