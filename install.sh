7bdc781f44ffdce3dfe3d0531ac204819bba27e4
Club09 s09t214 OOKAWA Masahiro <clc.iguto@gmail.com>
2014-04-26 16:54:38 +0900

    installスクリプトにgit関連ファイルを追加

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 relative_dir=`dirname $0`
 dotfiles_dir=`(cd $relative_dir; pwd)`
 echo $dotfiles_dir
-for file in .bashrc .dir_colors .inputrc .tmux.conf .vimrc .vim
+for file in .bashrc .dir_colors .inputrc .tmux.conf .vimrc .vim .gitignore .gitconfig
 do
   src=$dotfiles_dir/$file
   ln -fs $src $HOME
