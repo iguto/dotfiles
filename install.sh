732b9aa1cdbfe571cef352cd48401939ec826e12
Club09 s09t214 OOKAWA Masahiro <clc.iguto@gmail.com>
2014-08-25 19:41:17 +0900

    pecoの設定を追加

--- install.sh
+++ install.sh
@@ -3,25 +3,31 @@
 relative_dir=`dirname $0`
 dotfiles_dir=`(cd $relative_dir; pwd)`
 echo $dotfiles_dir
-for file in .bashrc .dir_colors .inputrc .tmux.conf .vimrc .vim .gitignore .gitconfig
-do
-  src=$dotfiles_dir/$file
-  ln -fs $src $HOME
-done
-
+#for file in .bashrc .dir_colors .inputrc .tmux.conf .vimrc .vim .gitignore .gitconfig
+#do
+#  src=$dotfiles_dir/$file
+#  ln -fs $src $HOME
+#done
 #
-# zsh 
+##
+## zsh 
+##
+#(
+#cd zsh.d
+#./install.sh
+#)
 #
-(
-cd zsh.d
-./install.sh
-)
-
-
 #
-# emacs
+##
+## emacs
+##
+#(
+#cd emacs.d
+#./install.sh
+#)
 #
-(
-cd emacs.d
-./install.sh
-)
+##
+## .config
+##
+[ ! -d ~/.config/peco ] && mkdir =p ~/.config/peco
+ln -sf $dotfiles_dir/.config/peco/config.json ~/.config/peco
