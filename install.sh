b339c7696f71f0c8d2ba117d56f627a0d6127ef9
iguto <clc.iguto@gmail.com>
2014-09-03 03:05:35 +0900

    install.shを更新

--- install.sh
+++ install.sh
@@ -1,33 +1,51 @@
-#!/bin/bash 
+#!/bin/bash
+
+for path in $(echo $PATH | sed 's/:/ /g')
+do
+  echo $path
+  [ "$path" = "$HOME/bin" ] && home_bin_exists=$path
+done
+[ ! -d $HOME/bin ] && mkdir $HOME/bin
+[ -z $home_bin_exists ] && export PATH=$PATH:$HOME/bin
+
+go_dir=$HOME/.golang
+[ ! -d $go_dir ] && mkdir $go_dir
+export GOPATH=$go_dir
+
+go get github.com/motemen/ghq
+ln -sf $GOPATH/bin/ghq $HOME/bin
+
+go get github.com/peco/peco/cmd/peco
+ln -sf $GOPATH/bin/peco $HOME/bin
 
 relative_dir=`dirname $0`
 dotfiles_dir=`(cd $relative_dir; pwd)`
 echo $dotfiles_dir
-#for file in .bashrc .dir_colors .inputrc .tmux.conf .vimrc .vim .gitignore .gitconfig
-#do
-#  src=$dotfiles_dir/$file
-#  ln -fs $src $HOME
-#done
+
+for file in .bashrc .dir_colors .inputrc .tmux.conf .vimrc .vim .gitignore .gitconfig
+do
+  src=$dotfiles_dir/$file
+  ln -fs $src $HOME
+done
+
 #
-##
-## zsh 
-##
-#(
-#cd zsh.d
-#./install.sh
-#)
+# zsh
 #
+(
+cd zsh.d
+./install.sh
+)
+
 #
-##
-## emacs
-##
-#(
-#cd emacs.d
-#./install.sh
-#)
+# emacs
 #
-##
-## .config
-##
-[ ! -d ~/.config/peco ] && mkdir =p ~/.config/peco
+(
+cd emacs.d
+./install.sh
+)
+
+#
+# .config
+#
+[ ! -d ~/.config/peco ] && mkdir -p ~/.config/peco
 ln -sf $dotfiles_dir/.config/peco/config.json ~/.config/peco
