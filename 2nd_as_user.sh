c2250e426f2428042a91a16926799ab842f5e07c
iguto <clc.iguto@gmail.com>
2014-09-19 00:13:35 +0900

    スクリプトでPATHの上書きがあったので修正

--- 2nd_as_user.sh
+++ 2nd_as_user.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # $ source THIS_FILE
-
+[ ! -d $HOME/.golang] && mkdir $HOME/.golang
 export GOPATH=$HOME/.golang
 go get github.com/motemen/ghq
 go get github.com/peco/peco/cmd/peco
 
-export PATH=PATH:$GOPATH/bin 
+export PATH=$PATH:$GOPATH/bin 
 
