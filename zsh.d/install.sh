78f15f89dfc4619aa4add2206e937f896d2f1241
iguto <clc.iguto@gmail.com>
2015-12-04 18:55:04 +0900

    fix install script

--- zsh.d/install.sh
+++ zsh.d/install.sh
@@ -3,12 +3,12 @@
 base_path=`pwd`
 echo "creating symlinks.."
 
-#ln -sf $base_path/.zshrc $HOME
-#ln -sf $base_path/.zshenv $HOME
-#
-#git submodule init
-#git submodule update
-#
+ln -sf $base_path/.zshrc $HOME
+ln -sf $base_path/.zshenv $HOME
+
+git submodule init
+git submodule update
+
 if [ -d $base_path/zsh-completions ]; then
   rm -rf $base_path/zsh-completions
 fi
